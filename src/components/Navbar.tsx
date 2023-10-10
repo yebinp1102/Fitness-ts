8fe6f3c61ea8f54a12f4c3fde478bac9b4cd607d
yebin Park <yebinp1102@naver.com>
2023-10-10 15:21:34 +0900

    Feat : toggle menu btn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Logo from "../assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "../shared/types";
 import useMediaQuery from "../hooks/useMediaQuery";
+import ActionButton from "../shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -17,12 +18,6 @@ const Navbar = ({selectedPage, setSelectedPage} : Props) => {
 
   return (
     <nav>
-      {/* fixed : position fix
-        top: 0
-        z-index: 30 - being on top
-        w-full : entire width 
-        py-6 : padding 6 level
-      */}
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
 
         {/* w-5/6 : 5/6% of width */}
@@ -43,12 +38,12 @@ const Navbar = ({selectedPage, setSelectedPage} : Props) => {
                   <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
                   <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
                   <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                  <Link page="Contact Us"selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                  <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
                 </div>
 
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a Member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
                 </div>
 
             </div>
@@ -64,6 +59,27 @@ const Navbar = ({selectedPage, setSelectedPage} : Props) => {
           </div>
         </div>
       </div>
+
+      {/* Mobile menu modal */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          
+          {/* Close icon */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+
+          {/* Menu items */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+            <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+            <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+            <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+          </div>
+        </div>
+      )}
     </nav>
   )
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,5 +2,5 @@ export enum SelectedPage {
   Home = "home",
   Benefits = "benefits",
   OurClasses = "ourclasses",
-  ContactUse = "contactus"
+  ContactUs = "contactus"
 }
